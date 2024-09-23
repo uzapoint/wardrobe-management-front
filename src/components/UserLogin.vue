41b55d28205d6e01541c87e7a4a0b7526028962c
LeinadCybre1024 <leinadcybre@gmail.com>
2024-09-23 11:07:11 +0300

    added tailwindcss and main.css , userlogin page change from helloworld

---
+++ src/components/UserLogin.vue
@@ -0,0 +1,63 @@
+<template>
+ <div class="bg-gray-100 min-h-screen flex items-center justify-center">
+    <!-- Background Image -->
+    <div class="absolute inset-0 z-0">
+        <img src="https://source.unsplash.com/random/1920x1080" alt=""
+            class="w-full h-full object-cover filter blur-lg brightness-50">
+    </div>
+
+    <!-- Login Form -->
+    <div class="relative z-10 bg-white p-8 rounded-md shadow-lg">
+        <h1 class="text-xl font-bold mb-4">Login</h1>
+        <form action="#" method="POST">
+            <div class="mb-4">
+                <label class="block text-gray-700 font-bold mb-2" for="email">Email</label>
+                <input
+                    class="appearance-none border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full"
+                    id="email" type="email" placeholder="Email">
+            </div>
+            <div class="mb-4">
+                <label class="block text-gray-700 font-bold mb-2" for="password">Password</label>
+                <input
+                    class="appearance-none border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full"
+                    id="password" type="password" placeholder="Password">
+            </div>
+            <div class="flex items-center justify-between gap-8">
+                <button
+                    class="bg-cyan-500 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    type="button">
+                    Sign In
+                </button>
+                <a class="inline-block align-baseline font-bold text-sm text-cyan-500 hover:text-cyan-800"
+                    href="#">
+                    Forgot Password?
+                </a>
+            </div>
+        </form>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'UserLogin',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
