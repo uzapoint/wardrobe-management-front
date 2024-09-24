229a0de52eccf798fb8b25bf378429c5324b824f
Donstesh <stephenshifoko@gmail.com>
2024-09-24 14:19:42 +0300

    Components

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import 'bootstrap'; // Import Bootstrap JS
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app');
